5eb34d6020a1e694032bcc995b63b632b94475a6
sawyerli <13894705977@163.com>
2019-07-01 18:10:26 +0800

    blog add headImg

--- models/doc.db.js
+++ models/doc.db.js
@@ -1,6 +1,6 @@
 const connect = require("./utils/connect");
 const { logger } = require("../tool/log");
-//查询所有文章标题
+//查询所有文章标题及用户名
 exports.getAllDocTitle = function(callback) {
   const _sql =
     `SELECT sys_blog.id,sys_blog.title, sys_blog.create_time, sys_user.username as author
@@ -17,12 +17,16 @@ exports.getAllDocTitle = function(callback) {
       callback({ type: 500 });
     });
 };
-//根据id查询文章详情
+
+//根据id查询文章及用户信息
 exports.getDocItems = function(id, callback) {
-  const _sql = "select * from sys_blog where id = ?";
-  const _addSqlParam = [id];
+  const _sql =
+    `SELECT sys_blog.id,sys_blog.title, sys_blog.create_time, sys_blog.create_time, sys_blog.dochtml,  
+      sys_user.username as author, sys_user.head_url 
+      FROM sys_blog INNER JOIN  sys_user  on sys_blog.account_id=sys_user.id
+      WHERE sys_blog.id =${id}`;
   connect
-    .querySQL(_sql, _addSqlParam)
+    .querySQL(_sql)
     .then((rows) => {
       callback(null, rows);
     })
