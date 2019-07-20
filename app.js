4f9219a83fd384d79bf91c8c3c5f1eaa3f0f2384
Sawyer-li <13894705977@163.com>
2019-07-20 12:01:40 +0800

    允许多种header

--- app.js
+++ app.js
@@ -10,11 +10,10 @@ const api = require('./controller');
 
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Access-Control-Request-Method,Access-Control-Request-Headers');
   res.header('Access-Control-Allow-Methods', '*');
   res.header('Content-Type', 'application/json;charset=utf-8');
   next();
-});
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(session({               //session的设置
